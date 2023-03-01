a2d109bb1d9b990ff6309f8946879384a26f3244
성중원 <jungwon.seong@seongjung-won-ui-MacBookPro.local>
2023-03-02 02:15:14 +0900

    feat: add emotion cofig

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,15 @@
+import styled from "@emotion/styled";
 import { Button } from "ui";
+
+const Component = styled.div`
+  background-color: red;
+`;
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
+      <Component>Web</Component>
       <Button />
     </div>
   );
